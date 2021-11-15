a9b6bfdc55d883cf18ed4deca3afed7f1e5b2a3e
Michele Stieven <usergalileo@gmail.com>
2021-11-15 21:09:31 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, combineLatest, ReplaySubject } from 'rxjs';
+import { map, share, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    {{ (user$ | async)?.username }}
+    {{ (user$ | async)?.username }}
+    {{ (user$ | async)?.username }}
+
+    <div *ngIf="showSecondDiv">
+      Second div
+      {{ (user$ | async)?.username }}
+    </div>
+
+    <button (click)="userId$.next('2')">+</button>
+  `,
 })
 export class AppComponent {
-  title = 'ng-playground';
+
+  showSecondDiv = false;
+
+  userId$ = new BehaviorSubject<string | null>('1');
+
+  user$ = this.userId$.pipe(
+    switchMap(userId =>
+      this.http.get<any>('https://jsonplaceholder.typicode.com/users/' + userId)
+    ),
+    share({
+
+    })
+  )
+
+  constructor(private http: HttpClient) {
+    setTimeout(() => {
+      this.showSecondDiv = true;
+    }, 3000)
+  }
 }
